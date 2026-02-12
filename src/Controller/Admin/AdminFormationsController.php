866635ec96ffe7362cff10669b4c33ef6bced573
Asukiri1 <ducfilippo@gmail.com>
2026-02-12 02:58:09 +0100

    Ajout des boutons et début de la partie admin

---
+++ src/Controller/Admin/AdminFormationsController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Repository\CategorieRepository;
+use App\Repository\FormationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminFormationsController extends AbstractController
+{
+    /**
+     * @var FormationRepository
+     */
+    private $formationRepository;
+    
+    /**
+     * @var CategorieRepository
+     */
+    private $categorieRepository;
+    
+    public function __construct(FormationRepository $formationRepository, CategorieRepository $categorieRepository) {
+        $this->formationRepository = $formationRepository;
+        $this->categorieRepository = $categorieRepository;
+    }
+    
+    #[Route('/admin/formations', name: 'admin.formations')]
+    public function index(): Response
+    {
+        $formations = $this->formationRepository->findAll();
+        $categories = $this->categorieRepository->findAll();
+        
+        return $this->render('admin/admin_formations/index.html.twig', [
+            'formations' => $formations,
+            'categories' => $categories
+        ]);
+    }
+
+    
+}
