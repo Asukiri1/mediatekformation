79d2e34f0d4ac22b95613062791e3afee93152e4
Asukiri1 <ducfilippo@gmail.com>
2026-02-13 03:27:12 +0100

    Ajout modif et supression des playlist

---
+++ src/Controller/Admin/AdminPlaylistsController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Playlist;
+use App\Form\PlaylistType;
+use App\Repository\CategorieRepository;
+use App\Repository\PlaylistRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminPlaylistsController extends AbstractController
+{
+    private $playlistRepository;
+    private $categorieRepository;
+    
+    public function __construct(PlaylistRepository $playlistRepository, CategorieRepository $categorieRepository) {
+        $this->playlistRepository = $playlistRepository;
+        $this->categorieRepository = $categorieRepository;
+    }
+    
+    #[Route('/admin/playlists', name: 'admin.playlists')]
+    public function index(): Response
+    {
+        $playlists = $this->playlistRepository->findAllOrderByName('ASC');
+        $categories = $this->categorieRepository->findAll();
+        
+        return $this->render('admin/admin_playlists/index.html.twig', [
+            'playlists' => $playlists,
+            'categories' => $categories
+        ]);
+    }
+
+    #[Route('/admin/playlists/suppr/{id}', name: 'admin.playlist.suppr')]
+    public function suppr(int $id, Request $request): Response
+    {
+        $playlist = $this->playlistRepository->find($id);
+
+        if ($this->isCsrfTokenValid('delete'.$playlist->getId(), $request->get('_token'))) {
+            
+            if (count($playlist->getFormations()) > 0) {
+                $this->addFlash('danger', 'Vous ne pouvez pas supprimer cette playlist car elle contient des formations.');
+            } else {
+                $this->playlistRepository->remove($playlist);
+                $this->addFlash('success', 'La playlist a été supprimée avec succès');
+            }
+        }
+
+        return $this->redirectToRoute('admin.playlists');
+    }
+    
+    #[Route('/admin/playlists/ajout', name: 'admin.playlist.ajout')]
+    public function ajout(Request $request): Response
+    {
+        $playlist = new Playlist();
+        $form = $this->createForm(PlaylistType::class, $playlist);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $this->playlistRepository->add($playlist);
+            return $this->redirectToRoute('admin.playlists');
+        }
+
+        return $this->render('admin/admin_playlists/formplaylist.html.twig', [
+            'formplaylist' => $form->createView()
+        ]);
+    }
+    
+    #[Route('/admin/playlists/edit/{id}', name: 'admin.playlist.edit')]
+    public function edit(int $id, Request $request): Response
+    {
+        $playlist = $this->playlistRepository->find($id);
+        $form = $this->createForm(PlaylistType::class, $playlist);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $this->playlistRepository->add($playlist);
+            return $this->redirectToRoute('admin.playlists');
+        }
+
+        return $this->render('admin/admin_playlists/formplaylist.html.twig', [
+            'formplaylist' => $form->createView(),
+            'playlist' => $playlist
+        ]);
+    }
+
+}
